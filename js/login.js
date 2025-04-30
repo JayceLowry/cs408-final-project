45fe163be566996a724061092617a0aee633bf34
Jayce Lowry <jaycelowry@u.boisestate.edu>
2025-04-30 13:50:11 -0600

    Improve onboarding

--- js/login.js
+++ js/login.js
@@ -22,14 +22,16 @@ export function sayHello() {
 }
 
 const loginForm = document.getElementById("login-form");
+const togglePasswordButton = document.getElementById("toggle-show");
+const passwordField = document.getElementById("password");
 
 loginForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 
     const usernameField = document.getElementById("username");
     const username = usernameField.value.trim();
-    const passwordField = document.getElementById("password");
     const password = passwordField.value.trim();
+    const loginError = document.getElementById("login-error");
 
     try {
         const response = await fetch("https://w450sz6yzd.execute-api.us-east-2.amazonaws.com/login", {
@@ -46,15 +48,25 @@ loginForm.addEventListener("submit", async function(event) {
         const data = await response.json();
 
         if (!response.ok) {
-            alert("Invalid username or password.");
-            throw new Error("Login Failed:", data.message);
+            loginError.hidden = false;
+            throw new Error("Login Failed:", data);
         }
+        loginError.hidden = true;
         console.log("Login successful:", data);
         sessionStorage.setItem("userId", data.userId);
         sessionStorage.setItem("username", data.username);
         window.location.href = "/dashboard.html";
     } catch (error) {
-      console.error(error);
-      alert("Something went wrong");
+        console.error(error);
     }
+});
+
+togglePasswordButton.addEventListener("click", function() {
+    const isHidden = passwordField.type === "password";
+    const eyeIcon = document.getElementById("eye");
+
+    passwordField.type = isHidden ? "text" : "password";
+    eyeIcon.src = isHidden ? "/img/eye.svg" : "/img/eye-slash.svg";
+    togglePasswordButton.title = isHidden ? "Hide Password" : "Show Password";
+    togglePasswordButton.setAttribute("aria-label", isHidden ? "Hide Password" : "Show Password");
 });
--- js/signup.js
+++ js/signup.js
@@ -22,14 +22,16 @@ export function sayHello() {
 }
 
 const signupForm = document.getElementById("signup-form");
+const passwordField = document.getElementById("password");
+const togglePasswordButton = document.getElementById("toggle-show");
 
 signupForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 
     const usernameField = document.getElementById("username");
     const username = usernameField.value.trim();
-    const passwordField = document.getElementById("password");
     const password = passwordField.value.trim();
+    const signupError = document.getElementById("signup-error");
 
     try {
         const response = await fetch("https://w450sz6yzd.execute-api.us-east-2.amazonaws.com/signup", {
@@ -46,9 +48,11 @@ signupForm.addEventListener("submit", async function(event) {
         const data = await response.json();
 
         if (!response.ok) {
-            alert("User already exists");
+            signupError.hidden = false;
+            signupForm.reset();
             throw new Error("Account Creation Failed:", data.message);
         }
+        signupError.hidden = true;
         console.log("Login successful:", data);
         sessionStorage.setItem("userId", data.userId);
         sessionStorage.setItem("username", data.username);
@@ -56,4 +60,14 @@ signupForm.addEventListener("submit", async function(event) {
     } catch (error) {
         console.error(error);
     }
+});
+
+togglePasswordButton.addEventListener("click", function() {
+    const isHidden = passwordField.type === "password";
+    const eyeIcon = document.getElementById("eye");
+
+    passwordField.type = isHidden ? "text" : "password";
+    eyeIcon.src = isHidden ? "/img/eye.svg" : "/img/eye-slash.svg";
+    togglePasswordButton.title = isHidden ? "Hide Password" : "Show Password";
+    togglePasswordButton.setAttribute("aria-label", isHidden ? "Hide Password" : "Show Password");
 });
